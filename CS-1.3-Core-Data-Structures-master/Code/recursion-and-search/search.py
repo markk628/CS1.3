52ec1e2255de211ffb9964271c1d005784c8aff4
Donghyun <markk628@gmail.com>
2020-04-10 15:02:56 -0700

    added binery iterative search

--- CS-1.3-Core-Data-Structures-master/Code/recursion-and-search/search.py
+++ CS-1.3-Core-Data-Structures-master/Code/recursion-and-search/search.py
@@ -4,7 +4,7 @@ def linear_search(array, item):
     """return the first index of item in array or None if item is not found"""
     # implement linear_search_iterative and linear_search_recursive below, then
     # change this to call your implementation to verify it passes all tests
-    return linear_search_iterative(array, item)
+    return linear_search_recursive(array, item)
     # return linear_search_recursive(array, item)
 
 
@@ -22,6 +22,14 @@ def linear_search_recursive(array, item, index=0):
     # once implemented, change linear_search to call linear_search_recursive
     # to verify that your recursive implementation passes all tests
 
+    if index >= len(array):
+        return None
+
+    if array[index] == item:
+        return index
+    else:
+        return linear_search_recursive(array, item, index+1)
+        
 
 def binary_search(array, item):
     """return the index of item in sorted array or None if item is not found"""
@@ -37,9 +45,25 @@ def binary_search_iterative(array, item):
     # once implemented, change binary_search to call binary_search_iterative
     # to verify that your iterative implementation passes all tests
 
+    leftIndex = 0
+    rightIndex = len(array) - 1
+
+    while leftIndex <= rightIndex:
+        midIndex = (rightIndex + leftIndex) // 2
+        if array[midIndex] == item:
+            return midIndex
+        elif item < array[midIndex]:
+            rightIndex = midIndex - 1
+        elif item  > array[midIndex]:
+            leftIndex = midIndex + 1
+    return None
+
+
 
 def binary_search_recursive(array, item, left=None, right=None):
     # TODO: implement binary search recursively here
     pass
     # once implemented, change binary_search to call binary_search_recursive
     # to verify that your recursive implementation passes all tests
+
+
