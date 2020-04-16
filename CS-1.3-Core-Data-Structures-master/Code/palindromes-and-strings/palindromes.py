6f00ff41da31739caa5c8f375a1f1b85feca8269
Donghyun <markk628@gmail.com>
2020-04-16 15:34:12 -0700

    working on palindroms

--- CS-1.3-Core-Data-Structures-master/Code/palindromes-and-strings/palindromes.py
+++ CS-1.3-Core-Data-Structures-master/Code/palindromes-and-strings/palindromes.py
@@ -22,14 +22,29 @@ def is_palindrome_iterative(text):
     pass
     # once implemented, change is_palindrome to call is_palindrome_iterative
     # to verify that your iterative implementation passes all tests
+    left_index = 0
+    right_index = len(text) - 1
 
+    while left_index <= right_index:
+        if text[left_index].lower() != text[right_index].lower():
+            return False
+        left_index += 1
+        right_index -= 1
+    return True
 
 def is_palindrome_recursive(text, left=None, right=None):
     # TODO: implement the is_palindrome function recursively here
     pass
     # once implemented, change is_palindrome to call is_palindrome_recursive
     # to verify that your iterative implementation passes all tests
-
+    if text[left].lower() != text[right].lower():
+        return False
+    if len(text) == 0 or len(text) == 1:
+        return True
+    
+    if left == right or left > right:
+        return True
+    return is_palindrome_recursive(text, left + 1, right - 1)
 
 def main():
     import sys
